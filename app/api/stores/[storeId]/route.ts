7e2e1160b7e520a2580526a856055c037eb7e017
DiegoxK <diego.stv@outlook.es>
2024-03-18 10:41:20 -0500

    Fixed PATCH and DELETE routes.

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -3,35 +3,6 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
-  _req: Request,
-  { params }: { params: { storeId: string } }
-) {
-  try {
-    const { userId } = auth();
-
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 });
-    }
-
-    if (!params.storeId) {
-      return new NextResponse("Store ID is required", { status: 400 });
-    }
-
-    const store = await prismadb.store.deleteMany({
-      where: {
-        id: params.storeId,
-        userId,
-      },
-    });
-
-    return NextResponse.json(store);
-  } catch (error) {
-    console.log("[STORE_DELETE]", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
-  }
-}
-
-export async function DELETE(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
@@ -66,6 +37,35 @@ export async function DELETE(
     return NextResponse.json(store);
   } catch (error) {
     console.log("[STORE_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store ID is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.deleteMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_DELETE]", error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
