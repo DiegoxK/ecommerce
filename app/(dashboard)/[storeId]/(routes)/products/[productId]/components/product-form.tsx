7948a2cdbe3f4bb16a89742e91d1c7ef727984f6
DiegoxK <diego.stv@outlook.es>
2024-04-02 08:51:57 -0500

    Added product pages

---
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -0,0 +1,170 @@
+"use client";
+import AlertModal from "@/components/modals/alert-modal";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import Heading from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import ImageUpload from "@/components/ui/image-upload";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Product } from "@prisma/client";
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import * as z from "zod";
+
+const formSchema = z.object({
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+});
+
+type ProductFormValues = z.infer<typeof formSchema>;
+
+interface ProductFormProps {
+  initialData: Product | null;
+}
+
+export default function ProductForm({ initialData }: ProductFormProps) {
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit Product" : "Create Product";
+  const description = initialData ? "Edit Product" : "Add a new product.";
+  const toastMessage = initialData ? "Product updated" : "Product created.";
+  const action = initialData ? "Save Changes" : "Create";
+
+  const form = useForm<ProductFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData ?? {
+      label: "",
+      imageUrl: "",
+    },
+  });
+
+  const onSubmit = async (data: ProductFormValues) => {
+    try {
+      setLoading(true);
+
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/products/${params.productId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/products`, data);
+      }
+
+      // router.refresh();
+      // router.push(`/${params.storeId}/products`);
+      window.location.assign(`/${params.storeId}/products`);
+      toast.success(toastMessage);
+    } catch (error) {
+      toast.error("An error occurred. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/products/${params.productId}`);
+      router.refresh();
+      router.push(`/${params.storeId}/products`);
+      toast.success("Product deleted.");
+    } catch (error) {
+      toast.error(
+        "Make sure you removed all categories using this product first."
+      );
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="icon"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background image</FormLabel>
+                <FormControl>
+                  {/* TODO: Delete images from hosting */}
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Label</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Product label"
+                      disabled={loading}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { ProductColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import ApiList from "@/components/ui/api-list";
+
+interface ProductClientProps {
+  data: ProductColumn[];
+}
+
+export default function ProductClient({ data }: ProductClientProps) {
+  const router = useRouter();
+  const params = useParams();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Products (${data.length})`}
+          description="Manage your products for your store."
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add new
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API call for products." />
+      <Separator />
+      <ApiList entityName="products" entityIdName="productId" />
+    </>
+  );
+}
