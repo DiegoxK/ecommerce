da954d0e668736d515772bfca9019a38b47bccd4
DiegoxK <diego.stv@outlook.es>
2024-04-08 11:39:41 -0500

    Update Home component to fetch and display billboard data

--- client/app/(routes)/page.tsx
+++ client/app/(routes)/page.tsx
@@ -1,11 +1,14 @@
+import getBillboard from "@/actions/get-billboard";
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
 
-export default function Home() {
+export default async function Home() {
+  const billboard = await getBillboard("52b79dd1-66fa-4a7e-8774-c7a33b37151b");
+
   return (
     <Container>
       <div>
-        <Billboard />
+        <Billboard data={billboard} />
       </div>
     </Container>
   );
