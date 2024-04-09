799161d0377315e5204aaf108125e1e26bab1df0
DiegoxK <diego.stv@outlook.es>
2024-04-09 09:59:26 -0500

    Change Home component styles

--- client/app/(routes)/page.tsx
+++ client/app/(routes)/page.tsx
@@ -10,11 +10,11 @@ export default async function Home() {
 
   return (
     <Container>
-      <div>
+      <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
-      </div>
-      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-        <ProductList title="Featured Products" products={products} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" products={products} />
+        </div>
       </div>
     </Container>
   );
