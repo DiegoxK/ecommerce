e03d5376b2defbf29fc2df1ff91481fa26c156fb
DiegoxK <diego.stv@outlook.es>
2024-04-10 06:13:29 -0500

    Added image panels

--- client/components/gallery/index.tsx
+++ client/components/gallery/index.tsx
@@ -12,7 +12,7 @@ interface GalleryProps {
 
 export default function Gallery({ images }: GalleryProps) {
   return (
-    <Tab.Group as="div">
+    <Tab.Group as="div" className="flex flex-col-reverse">
       <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
         <Tab.List className="grid grid-cols-4 gap-6">
           {images.map((image) => (
@@ -20,6 +20,20 @@ export default function Gallery({ images }: GalleryProps) {
           ))}
         </Tab.List>
       </div>
+      <Tab.Panels className="aspect-square w-full">
+        {images.map((image) => (
+          <Tab.Panel key={image.id}>
+            <div className="aspect-square relative h-full w-full sm:rounded-lg overflow-hidden">
+              <Image
+                fill
+                src={image.url}
+                alt="Product image"
+                className="object-cover object-center"
+              />
+            </div>
+          </Tab.Panel>
+        ))}
+      </Tab.Panels>
     </Tab.Group>
   );
 }
